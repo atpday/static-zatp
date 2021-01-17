eb6a3837413eabbc206ee78a879da09c72365307
atp <atpday@gmail.com>
2021-01-17 18:15:14 +0800

    update content.js to catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -1,17 +1,17 @@
 $(function(){
-    var navH = $("#post-toc").offset().top;
+    var navH = $("#catalog").offset().top;
     //滚动条事件
     $(window).scroll(function(){
         //获取滚动条的滑动距离
         var scroH = $(this).scrollTop();
         //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定
         if(scroH>=navH){
-            $(".post-toc").css({position: 'fixed',width: '256px',visibility: 'visible'});
-            $(".post-toc").animate({top: '90px'},800);
+            $(".catalog").css({position: 'fixed',width: '256px',visibility: 'visible'});
+            $(".catalog").animate({top: '90px'},800);
         }
         else{
-            $(".post-toc").css({position: 'static',top:'0px',visibility: 'hidden'});
-            $(".post-toc").animate(800);
+            $(".catalog").css({position: 'static',top:'0px',visibility: 'hidden'});
+            $(".catalog").animate(800);
         }
     });
 
